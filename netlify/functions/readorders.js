386b45891dce8ecc569fe25aecbca30ee14744f3
shiyunkhoo <shiyun.khoo@gmail.com>
2022-08-09 12:33:15 +0800

    firebase

---
+++ netlify/functions/readorders.js
@@ -0,0 +1,38 @@
+const admin = require('firebase-admin');
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const serviceAccount = JSON.parse(process.env.firebaseCredential);
+
+exports.handler = async function (event, context) {
+  const user = context.clientContext.user;
+
+  if(user.email != Shiyun.khoo@gmail.com) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        message: "You are not allowed to retrieve orders",
+      }),
+    }
+  };
+
+  let app;
+  if (admin.apps.length === 0) {
+    app = initializeApp({
+      credential: cert(serviceAccount),
+    });
+  }
+
+  const db = getFirestore(app);
+  const orders = await db.collection('orders').get();
+
+  const results = orders.docChanges.map((doc) => {
+    return {
+      id: doc.id,
+      data: doc.data(),
+    };
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify(results),
+  };
+};
